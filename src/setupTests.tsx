4d2e49575faceb9d8fbb847ee70e5789548525a2
lizraeli <leo2002b@yahoo.com>
2020-04-09 22:13:07 -0400

    tests setup file

---
+++ src/setupTests.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import type { Option } from "./types";
+
+// Mocking react-select as a regular select element 
+// see https://www.polvara.me/posts/testing-a-custom-select-with-react-testing-library/
+type OnChangeSelect = (option: Option) => void;
+jest.mock(
+  "react-select",
+  () => (props: {
+    id: string;
+    options: Option[];
+    value: string;
+    onChange: OnChangeSelect;
+  }) => {
+    const { id, options, value, onChange } = props;
+    function handleChange(event: React.ChangeEvent<HTMLSelectElement>) {
+      const option = options.find(
+        (option) => option.value === event.currentTarget.value
+      );
+      if (option) {
+        onChange(option);
+      }
+    }
+    return (
+      <select data-testid={id} value={value || ""} onChange={handleChange}>
+        {options.map(({ label, value }) => (
+          <option key={value} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+    );
+  }
+);
